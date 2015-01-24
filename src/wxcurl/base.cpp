b0e7e1dd9b9c877614f68590c2ec75eb637a29f1
Pavel Kalian <pavel@kalian.cz>
2015-01-24 14:09:12 -0600

    Replace tabs with spaces

--- src/wxcurl/base.cpp
+++ src/wxcurl/base.cpp
@@ -792,8 +792,8 @@ void wxCurlBase::SetCurlHandleToDefaults(const wxString& relativeURL)
         SetOpt(CURLOPT_ERRORBUFFER, m_szDetailedErrorBuffer);
         
         SetOpt(CURLOPT_USERAGENT, "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:35.0) Gecko/20100101 Firefox/35.0\r\n" \
-                      "Accept: application/xml,text/html,application/xhtml+xml;q=0.9,*/*;q=0.8\r\n" \
-					  "Connection: keep-alive"); //Pretend we are a normal browser
+                    "Accept: application/xml,text/html,application/xhtml+xml;q=0.9,*/*;q=0.8\r\n" \
+                    "Connection: keep-alive"); //Pretend we are a normal browser
         SetOpt(CURLOPT_FOLLOWLOCATION, 1L);
 #ifdef __WXMSW__
         SetOpt(CURLOPT_CAINFO, "curl-ca-bundle.crt"); //Use our local certificate list on Windows
