7e8e5301ad48345485bfa84c6476e6773b8324a0
Pavel Kalian <pavel@kalian.cz>
2015-04-14 21:00:49 -0500

    Reformat the chart catalog source code to follow OpenCPN whitespace conventions

--- src/chartcatalog.h
+++ src/chartcatalog.h
@@ -55,218 +55,218 @@ WX_DECLARE_OBJARRAY(Chart, wxArrayOfCharts);
 class ChartCatalog
 {
 public:
-      ChartCatalog();
-      ~ChartCatalog();
-      // public methods
-      bool LoadFromFile(wxString path, bool headerOnly = false);
-      bool LoadFromXml(TiXmlDocument * doc, bool headerOnly);
-      wxDateTime GetReleaseDate( void );
-      // public properties
-      wxString title;
-      wxDateTime date_created;
-      wxDateTime time_created;
-      wxDateTime date_valid;
-      wxDateTime time_valid;
-      wxDateTime dt_valid;
-      wxString ref_spec;
-      wxString ref_spec_vers;
-      wxString s62AgencyCode;
-      wxArrayOfCharts *charts;
+    ChartCatalog();
+    ~ChartCatalog();
+    // public methods
+    bool LoadFromFile( wxString path, bool headerOnly = false );
+    bool LoadFromXml( TiXmlDocument * doc, bool headerOnly );
+    wxDateTime GetReleaseDate( void );
+    // public properties
+    wxString title;
+    wxDateTime date_created;
+    wxDateTime time_created;
+    wxDateTime date_valid;
+    wxDateTime time_valid;
+    wxDateTime dt_valid;
+    wxString ref_spec;
+    wxString ref_spec_vers;
+    wxString s62AgencyCode;
+    wxArrayOfCharts *charts;
 private:
-      bool ParseNoaaHeader(TiXmlElement * xmldata);
+    bool ParseNoaaHeader( TiXmlElement * xmldata );
 };
 
 class Chart
 {
 public:
-      Chart(TiXmlNode * xmldata);
-      ~Chart();
-      // public methods
-      virtual wxString GetChartTitle() { return title; }
-      virtual wxString GetDownloadLocation() { return zipfile_location; }
-      virtual bool NeedsManualDownload() { return manual_download_url != wxEmptyString; }
-      virtual wxString GetManualDownloadUrl() { return manual_download_url; }
-      virtual wxString GetChartFilename(bool to_check = false);
-      virtual wxDateTime GetUpdateDatetime() { return zipfile_datetime_iso8601; }
+    Chart( TiXmlNode * xmldata );
+    ~Chart();
+    // public methods
+    virtual wxString GetChartTitle() { return title; }
+    virtual wxString GetDownloadLocation() { return zipfile_location; }
+    virtual bool NeedsManualDownload() { return manual_download_url != wxEmptyString; }
+    virtual wxString GetManualDownloadUrl() { return manual_download_url; }
+    virtual wxString GetChartFilename( bool to_check = false );
+    virtual wxDateTime GetUpdateDatetime() { return zipfile_datetime_iso8601; }
 
-      // public properties
-      wxString number;
-      wxString title; //RNC: <title>, ENC:<lname>
-      wxArrayString *coast_guard_districts;
-      wxArrayString *states;
-      wxArrayString *regions;
-      wxString zipfile_location;
-      wxString target_filename;
-      wxDateTime zipfile_datetime;
-      wxDateTime zipfile_datetime_iso8601;
-      int zipfile_size;
-      wxString reference_file;
-      wxString manual_download_url;
+    // public properties
+    wxString number;
+    wxString title; //RNC: <title>, ENC:<lname>
+    wxArrayString *coast_guard_districts;
+    wxArrayString *states;
+    wxArrayString *regions;
+    wxString zipfile_location;
+    wxString target_filename;
+    wxDateTime zipfile_datetime;
+    wxDateTime zipfile_datetime_iso8601;
+    int zipfile_size;
+    wxString reference_file;
+    wxString manual_download_url;
 
-      NoticeToMariners *nm;
-      NoticeToMariners *lnm;
-      wxArrayOfPanels *coverage;
+    NoticeToMariners *nm;
+    NoticeToMariners *lnm;
+    wxArrayOfPanels *coverage;
 };
 
 class RasterChart : public Chart //<chart>
 {
 public:
-      RasterChart(TiXmlNode * xmldata);
-      // public methods
+    RasterChart( TiXmlNode * xmldata );
+    // public methods
 
-      //public properties
-      int source_edition;
-      int raster_edition;
-      int ntm_edition;
-      wxString source_date;
-      wxString ntm_date;
-      wxString source_edition_last_correction;
-      wxString raster_edition_last_correction;
-      wxString ntm_edition_last_correction;
+    //public properties
+    int source_edition;
+    int raster_edition;
+    int ntm_edition;
+    wxString source_date;
+    wxString ntm_date;
+    wxString source_edition_last_correction;
+    wxString raster_edition_last_correction;
+    wxString ntm_edition_last_correction;
 };
 
 class EncCell : public Chart //<cell>
 {
 public:
-      EncCell(TiXmlNode * xmldata);
-      // public methods
+    EncCell( TiXmlNode * xmldata );
+    // public methods
 
-      //public properties
-      wxString name;
-      wxString src_chart;
-      int cscale;
-      wxString status;
-      int edtn;
-      int updn;
-      wxDateTime uadt;
-      wxDateTime isdt;
+    //public properties
+    wxString name;
+    wxString src_chart;
+    int cscale;
+    wxString status;
+    int edtn;
+    int updn;
+    wxDateTime uadt;
+    wxDateTime isdt;
 };
 
 class IEncCell : public Chart //<Cell>
 {
 public:
-      IEncCell(TiXmlNode * xmldata);
-      ~IEncCell();
-      // public methods
-      wxString GetChartTitle();
-      wxString GetDownloadLocation();
-      wxDateTime GetUpdateDatetime();
+    IEncCell( TiXmlNode * xmldata );
+    ~IEncCell();
+    // public methods
+    wxString GetChartTitle();
+    wxString GetDownloadLocation();
+    wxDateTime GetUpdateDatetime();
 
-      //public properties
-      wxString name;
-      Location *location;
-      wxString river_name;
-      RiverMiles *river_miles;
-      Area *area;
-      wxString edition;
-      ChartFile *shp_file;
-      ChartFile *s57_file;
-      ChartFile *kml_file;
+    //public properties
+    wxString name;
+    Location *location;
+    wxString river_name;
+    RiverMiles *river_miles;
+    Area *area;
+    wxString edition;
+    ChartFile *shp_file;
+    ChartFile *s57_file;
+    ChartFile *kml_file;
 };
 
 class ChartFile
 {
 public:
-      ChartFile(TiXmlNode * xmldata);
-      // public methods
+    ChartFile( TiXmlNode * xmldata );
+    // public methods
 
-      //public properties
-      wxString location;
-      wxDateTime date_posted;
-      wxDateTime time_posted;
-      int file_size;
+    //public properties
+    wxString location;
+    wxDateTime date_posted;
+    wxDateTime time_posted;
+    int file_size;
 };
 
 class Location
 {
 public:
-      Location(TiXmlNode * xmldata);
-      // public methods
+    Location( TiXmlNode * xmldata );
+    // public methods
 
-      //public properties
-      wxString from;
-      wxString to;
+    //public properties
+    wxString from;
+    wxString to;
 };
 
 class RiverMiles
 {
 public:
-      RiverMiles(TiXmlNode * xmldata);
-      // public methods
+    RiverMiles( TiXmlNode * xmldata );
+    // public methods
 
-      //public properties
-      double begin;
-      double end;
+    //public properties
+    double begin;
+    double end;
 };
 
 class Area
 {
 public:
-      Area(TiXmlNode * xmldata);
-      // public methods
+    Area( TiXmlNode * xmldata );
+    // public methods
 
-      //public properties
-      double north;
-      double south;
-      double east;
-      double west;
+    //public properties
+    double north;
+    double south;
+    double east;
+    double west;
 };
 
 class NoticeToMariners //for <nm> and <lnm>
 {
 public:
-      NoticeToMariners(TiXmlNode * xmldata);
-      // public methods
+    NoticeToMariners( TiXmlNode * xmldata );
+    // public methods
 
-      //public properties
-      wxString agency; //<nm_agency> or <lnm_agency>
-      wxString doc;
-      wxDateTime date;
+    //public properties
+    wxString agency; //<nm_agency> or <lnm_agency>
+    wxString doc;
+    wxDateTime date;
 };
 
 class Vertex
 {
 public:
-      Vertex(TiXmlNode * xmldata);
-      // public methods
+    Vertex( TiXmlNode * xmldata );
+    // public methods
 
-      //public properties
-      double lat;
-      double lon;
+    //public properties
+    double lat;
+    double lon;
 };
 
 class Panel
 {
 public:
-      Panel(TiXmlNode * xmldata);
-      ~Panel();
-      // public methods
+    Panel( TiXmlNode * xmldata );
+    ~Panel();
+    // public methods
 
-      //public properties
-      int panel_no;
-      wxArrayOfVertexes *vertexes;
+    //public properties
+    int panel_no;
+    wxArrayOfVertexes *vertexes;
 };
 
 class RncPanel : public Panel
 {
 public:
-      RncPanel(TiXmlNode * xmldata);
-      // public methods
+    RncPanel( TiXmlNode * xmldata );
+    // public methods
 
-      //public properties
-      wxString panel_title;
-      wxString file_name;
-      int scale;
+    //public properties
+    wxString panel_title;
+    wxString file_name;
+    int scale;
 };
 
 class EncPanel : public Panel
 {
 public:
-      EncPanel(TiXmlNode * xmldata);
-      // public methods
+    EncPanel( TiXmlNode * xmldata );
+    // public methods
 
-      //public properties
-      wxString type;
+    //public properties
+    wxString type;
 };
 
 #endif //_CHARTCATALOG_H_
